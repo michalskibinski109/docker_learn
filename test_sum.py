534f59a9fafea1378f99801b80a1bda17962dff2
michalskibinski109 <michalskibinski109@gmail.com>
2023-02-18 10:59:17 +0100

    updated workflows

--- test_sum.py
+++ test_sum.py
@@ -5,3 +5,7 @@ def test_sum_numbers():
     assert sum_numbers(1, 2, 3) == 6, "Should be 6"
     assert sum_numbers(8, 2, 3, 0, 7) == 20, "Should be 20"
     print("YOUR CODE IS CORRECT!")
+
+
+def test_fail():
+    assert False is True, "Should be True"
